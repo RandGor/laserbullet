156e8ba0688c494c3383cf77ae0bdb761051d59c
RandGor <amlqn0bkjqtv@mail.ru>
2020-01-03 21:51:44 +0700

    Add files via upload

---
+++ psysics/Assets/MobileCamera.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileCamera : MonoBehaviour
+{
+
+    float mainSpeed = 50.0f;
+    float shiftAdd = 250.0f;
+    float maxShift = 1000.0f;
+    float camSens = 0.25f;
+    private Vector3 lastMouse = new Vector3(255, 255, 255);
+    private float totalRun = 1.0f;
+
+    Vector3 FirstPoint;
+    Vector3 SecondPoint;
+    float xAngle;
+    float yAngle;
+    float xAngleTemp;
+    float yAngleTemp;
+
+    public GameObject panel;
+    public GameObject text;
+
+    void Start()
+    {
+        xAngle = 0;
+        yAngle = 0;
+        this.transform.rotation = Quaternion.Euler(yAngle, xAngle, 0);
+    }
+
+    void Update()
+    {
+        //text.GetComponent<UnityEngine.UI.Text>().text = (Input.touchCount).ToString();
+        
+        //if (Input.touchCount > 0)
+        //{
+        //    if (Input.GetTouch(0).phase == TouchPhase.Began)
+        //    {
+        //        FirstPoint = Input.GetTouch(0).position;
+        //        xAngleTemp = xAngle;
+        //        yAngleTemp = yAngle;
+        //    }
+        //    if (Input.GetTouch(0).phase == TouchPhase.Moved)
+        //    {
+        //        SecondPoint = Input.GetTouch(0).position;
+        //        xAngle = xAngleTemp + (SecondPoint.x - FirstPoint.x) * 180 / Screen.width;
+        //        yAngle = yAngleTemp + (SecondPoint.y - FirstPoint.y) * 90 / Screen.height;
+        //        this.transform.rotation = Quaternion.Euler(yAngle, -xAngle, 0.0f);
+        //    }
+        //}
+    }
+}
---
+++ psysics/Assets/NewBehaviourScript.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    float m = 10f;
+    float k = 0.47f;
+    float gx = 0f;
+    float gy = -10f;
+    float gz = 0f;
+    float x0 = 0f;
+    float y0 = 0f;
+    float z0 = 0f;
+    float v0x = 10f;
+    float v0y = 10f;
+    float v0z = 0f;
+    float dt = 0.1f;
+    float tmax = 10f;
+    // Start is called before the first frame update
+    void draw(float t, Color c, float size) {
+        float x = xt(t);
+        float y = yt(t);
+        float z = zt(t);
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = new Vector3(x, y, z);
+        sphere.transform.localScale -= new Vector3(1 - size, 1 - size, 1 - size);
+        var sphereRenderer = sphere.GetComponent<Renderer>();
+        
+        sphereRenderer.material.SetColor("_Color", c);
+    }
+
+
+
+    float xt(float t)
+    {
+        return x0 + m / k * ((v0x - m * gx / k) * (1 - Mathf.Exp(-k / m * t)) + gx * t);
+    }
+
+    float yt(float t)
+    {
+        return y0 + m / k * ((v0y - m * gy / k) * (1 - Mathf.Exp(-k / m * t)) + gy * t);
+    }
+
+    float zt(float t)
+    {
+        
+        return z0 + m / k * ((v0z - m * gz / k) * (1 - Mathf.Exp(-k / m * t)) + gz * t);
+    }
+
+    float vxt(float t)
+    {
+        return v0x * Mathf.Exp(-k / m * t) - gx * m / k * (1 - Mathf.Exp(-k / m * t));
+    }
+
+    float vyt(float t)
+    {
+        return v0y * Mathf.Exp(-k / m * t) + gy * m / k * (1 - Mathf.Exp(-k / m * t));
+    }
+
+    float vyz(float t)
+    {
+        return v0z * Mathf.Exp(-k / m * t) + gz * m / k * (1 - Mathf.Exp(-k / m * t));
+    }
+
+    public Slider mainSlider;
+    public GameObject bullet;
+
+
+    public void ValueChangeCheck()
+    {
+        float t = mainSlider.value;
+        float x = xt(t);
+        float y = yt(t);
+        float z = zt(t);
+        bullet.transform.position = new Vector3(x, y, z);
+    }
+
+    void Start()
+    {
+        mainSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        float width, height, multi;
+        multi = 5;
+        width = (float)(Screen.width*0.8) / 5;
+        height = 20;
+        mainSlider.GetComponent< RectTransform>().sizeDelta = new Vector2(width, height);
+        mainSlider.transform.localScale = new Vector3(multi, multi, 1);
+        float size = 1f;
+        int N = 20;
+        for (float t = 0; t <= tmax; t+=dt) {
+            draw(t, Color.red, size);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
